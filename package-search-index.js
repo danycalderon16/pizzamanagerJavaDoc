2d31c9590c0cdaf9323ce17bd1a37ad327520a79
Daniel Calderon <danycalvir@gmail.com>
2021-06-04 20:38:18 -0600

    v1

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"conexion"},{"l":"controladores"},{"l":"data"},{"l":"forms"},{"l":"interfaces"},{"l":"menus"},{"l":"modelos"},{"l":"subInterfaz"},{"l":"util"}]
